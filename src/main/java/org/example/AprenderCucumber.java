26b259cbf023cc4057f2a1a94c049c70e2b0565a
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2023-03-18 07:34:45 -0300

    Ajustes dos testes

---
+++ src/main/java/org/example/AprenderCucumber.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AprenderCucumber {
+
+    @Given("^que criei o arquivo corretamente$")
+    public void que_criei_o_arquivo_corretamente() throws Throwable {
+
+    }
+
+    @When("^Quando executá-lo$")
+    public void quando_executá_lo() throws Throwable {
+
+    }
+
+    @Then("^Então a especificação deve finalizar com sucesso$")
+    public void então_a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+
+    }
+}
